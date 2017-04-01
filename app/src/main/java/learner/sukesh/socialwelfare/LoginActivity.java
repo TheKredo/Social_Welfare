0f4b69f21ece171b89c7d6f37d5431975cb68be8
Sukesh Panwar <1421037@gmail.com>
2017-04-01 08:06:12 +0530

    Update LoginActivity.java

--- app/src/main/java/learner/sukesh/socialwelfare/LoginActivity.java
+++ app/src/main/java/learner/sukesh/socialwelfare/LoginActivity.java
@@ -68,8 +68,6 @@ public class LoginActivity extends AppCompatActivity {
         ConnectivityManager cm = (ConnectivityManager)
                 getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        // if no network is available networkInfo will be null
-        // otherwise check if we are connected
         if (networkInfo != null && networkInfo.isConnected()) {
             return true;
         }
